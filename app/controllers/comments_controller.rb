9215d42d1edc2702f03579672bff52587ce1026d
kazuhitonakayama <firstjin19990101@gmail.com>
2021-02-12 15:32:46 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Post.find(params[:post_id]).comments
+    @post = Post.find(params[:post_id])
+  end
+
+  def new
+    @comment = Comment.new
+    @post = Post.find_by(id: params[:post_id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+        redirect_to :root
+    else
+        render action: :new
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:content,:post_id)
+    end
 end
